69003cc1ebd7a927a755c805b185d1dcabac952f
Hicham-Azeroual <hichamazroual2002@gmail.com>
2024-11-09 15:16:35 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
-import express from 'express'
-const app=express();
-app.listen(3000,()=>{
-    console.log("Server is runing on port 5000");
-    
-});
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+const app = express();
+dotenv.config()
+// MongoDB connection URL
+const mongoURI = process.env.MONGO;
+
+// Connect to MongoDB using mongoose
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    // Start the Express server only after MongoDB connection is successful
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(err => {
+    console.log("MongoDB connection error:", err);
+  });
