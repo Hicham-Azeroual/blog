2908211ac21aacb679181af20f1dedc584dafd7c
Hicham-Azeroual <hichamazroual2002@gmail.com>
2024-11-10 11:57:23 +0100

    add amidlware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,23 +1,25 @@
 // api/routes/user.route.js
-import express from 'express';
-import bcrypt from 'bcryptjs'; // مكتبة لتشفير كلمات المرور
-import User from '../models/user.module.js';
+import express from "express";
+import bcrypt from "bcryptjs"; // مكتبة لتشفير كلمات المرور
+import User from "../models/user.module.js";
+import { errorHandler } from "../utils/error.js";
 
 const router = express.Router();
 
 // إنشاء مستخدم جديد (Sign Up)
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
+
     // التحقق من أن البيانات غير فارغة
     if (!username || !email || !password) {
-      return res.status(400).json({ message: 'Please fill in all fields' });
+      return next(errorHandler(400, 'All fields are required')); // أضف return لتجنب تنفيذ التعليمات التالية
     }
 
     // التحقق إذا كان البريد الإلكتروني موجوداً بالفعل
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'Email already in use' });
+      return res.status(400).json({ message: "Email already in use" });
     }
 
     // تشفير كلمة المرور
@@ -34,10 +36,10 @@ router.post('/signup', async (req, res) => {
     await newUser.save();
 
     // إرسال استجابة بنجاح
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    next(err);
   }
 });
 
